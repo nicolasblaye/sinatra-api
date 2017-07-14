0a6dffb272b0ff630b201a0c933ee9fdd859671a
Whitefret <nblaye@powerspace.com>
2017-07-14 13:30:06 +0200

    Solve first examples

--- app/api.rb
+++ app/api.rb
@@ -1,10 +1,22 @@
 require 'bundler/setup'
 require 'sinatra/base'
+require 'json'
 
 class Api < Sinatra::Base
 
+  file = File.read(Dir.pwd + "/data.json")
+  json = JSON.parse(file)
+
 	get '/hello' do
-		'hello world!'
+		'Hello world!'
 	end
+
+	get '/movies' do
+    json
+  end
+
+  get '/movies/:movieId' do |movieId|
+    json[movies][movieId - 1]
+  end
 
 end
